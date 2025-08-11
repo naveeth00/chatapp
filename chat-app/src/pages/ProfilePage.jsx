54cf44509b727cee4305f4701b77ccbe08b7906f
naveeth00 <naveethahmad35@gmail.com>
2025-08-11 22:54:59 +0530

    Ongoing

--- chat-app/src/pages/ProfilePage.jsx
+++ chat-app/src/pages/ProfilePage.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import assets from '../assets/assets'
+import { AuthContext } from '../../context/AuthContext.jsx'
 
 const ProfilePage = () => {
 
+  const {authUser, updateProfile} = useContext(AuthContext)
+
   const [selectedImage, setSelectedImage] = useState(null)
   const navigate = useNavigate();
-  const [name, setName] = useState("Martin Johnson")
-  const [bio, setBio] = useState("Hi Everyone, I am Using QuickChat")
+  const [name, setName] = useState(authUser.fullName)
+  const [bio, setBio] = useState(authUser.bio)
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    navigate('/')
+    if (!selectedImage){
+      await updateProfile({fullName: name, bio});
+      navigate('/');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.reasAsDataURL(selectedImage);
+    reader.onload = async ()=>{
+      const base64Image = reader.result;
+      await updateProfile({profilePic: base64Image, fullName: name, bio});
+      navigate('/');
+    }
+
+    
   }
 
   return (
@@ -20,7 +37,7 @@ const ProfilePage = () => {
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2
       border-gray-600 flex items-center justify-between max-sm:flex-col-reverse
       rounded-lg'>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
+        <form onSubmit={ handleSubmit } className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg '>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 
           cursor-pointer'>
@@ -38,10 +55,11 @@ const ProfilePage = () => {
           placeholder='Write profile bio' required className='p-2 
           border border-gray-500 rounded-md focus:outline-none focus:ring-2 
           focus:ring-violet-500' rows={4}></textarea>
-          <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600
+          <button type="submit" className='bg-gradient-to-r from-purple-400 to-violet-600
           text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
         </form>
-        <img className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' src={assets.logo_icon} alt="" />
+        <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImage && 
+          'rounded-full'}`} src={authUser?.profilePic || assets.logo_icon} alt="" />
       </div>
 
       
