e7d25409ddff912206fdb3765ddf7f5b53bb72b5
naveeth00 <naveethahmad35@gmail.com>
2025-09-09 15:17:27 +0530

    Ongoing

--- chat-app/src/components/Sidebar.jsx
+++ chat-app/src/components/Sidebar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import assets, { userDummyData } from '../assets/assets'
+import React, { useContext } from 'react'
+import assets from '../assets/assets'
 import {useNavigate} from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import { ChatContext } from '../../context/ChatContext';
 
-const Sidebar = ({selectedUser, setSelectedUser}) => {
+const Sidebar = () => {
 
-const navigate = useNavigate();
+  const {getUsers, users, selectedUser, setSelectedUser,
+    unseenMessages, setUnseenMessages } = useContext(ChatContext)
+
+  const {logout, onlineUsers} = useContext(AuthContext)
+
+  const  [input, setInput] = useState(false)
+
+  const navigate = useNavigate();
+
+  const filteredUsers = input ? users.filter((user)=>user.fullName.toLowerCase().
+ includes(input.toLowerCase())) : users;
+
+ useEffect(()=>{
+    getUsers();
+  },[onlineUsers])
 
   return (
     <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white
@@ -18,14 +34,15 @@ const navigate = useNavigate();
               border border-gray-600 text-gray-100 hidden group-hover:block'>
                 <p onClick={()=>navigate( '/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
                 <hr className='my-2 border-t border-gray-500' />
-                <p className='cursor-pointer text-sm'>Logout</p>
+                <p onClick={()=> logout()} className='cursor-pointer 
+                text-sm'>Logout</p>
               </div>
           </div>
         </div>
 
         <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5'>
           <img src={assets.search_icon} alt="Search" className='w-3' />
-          <input type="text" className='bg-transparent border-none outline-none text-white
+          <input onChange={(e)=>setInput(e.target)} type="text" className='bg-transparent border-none outline-none text-white
            text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...'/>
         </div>
 
@@ -33,7 +50,7 @@ const navigate = useNavigate();
       
 
       <div className='flex flex-col'>
-        {userDummyData.map((user, index)=>(
+        {filteredUsers.map((user, index)=>(
           <div onClick={() => {setSelectedUser(user)}} 
           key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded 
           cursor-pointer max-sm:text-sm ${selectedUser ?._id === user._id && 'bg-[#282142]/50'}`}>
@@ -43,16 +60,16 @@ const navigate = useNavigate();
             <div className='flex flex-col leading-5'>
               <p>{user.fullName}</p>
               {
-                index < 3
+                onlineUsers.includes(user._id)
                 ? <span className='text-green-400 text-xs'>Online</span>
                 : <span className='text-neutral-400 text-xs'>Ofline</span>
               }
 
             </div>
             {
-              index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5
+              unseenMessages[user._id] > 0 && <p className='absolute top-4 right-4 text-xs h-5 w-5
               flex justify-center items-center rounded-full bg-violet-500/50'>
-              {index}</p>
+              {unseenMessages[user._id]}</p>
             }
             <div/>
           </div>
